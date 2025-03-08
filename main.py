8288ace3d65e54678c3aed4d8c2b41b644f3616b
Serj8772 <icq66623702@ya.ru>
2025-03-08 09:45:31 +0300

    BS edited

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from settings import delay, elements_value, filename
 from source.CsvUser import CsvUser
 from source.SeleniumS import SeleniumS
 from source.TelegramBot import TelegramBot
+from source.BS import BS
 
 
 load_dotenv()
@@ -15,24 +16,32 @@ id_channel = os.getenv("ID_CHANNEL")
 link = os.getenv("LINK")
 
 
-if __name__ == '__main__':
-    tbot = TelegramBot(token)
-
-    while True:
+def main(selenium=False):
+    if selenium:
         selenium_bot = SeleniumS(headless=True)
         selenium_bot.get_page(link)
         selenium_bot.make_screenshot()
         attribute_list = selenium_bot.get_attributes_list(By.CLASS_NAME, elements_value, By.TAG_NAME, 'a', 'href')
-        csv_user = CsvUser(filename)
-        rows = csv_user.read_csv()
-        for attribute in attribute_list:
-            if [attribute] not in rows:
-                csv_user.write_csv([attribute])
-                print(attribute, '--', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-                tbot.send_message(id_channel, text=attribute)
-                time.sleep(5)
-            else:
-                print('строка уже существует', '--', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
         selenium_bot.close()
+    else:
+        bs = BS(link)
+        attribute_list = bs.get_links('iva-item-title-CdRXl', 'styles-module-root-m3BML')
 
+    csv_user = CsvUser(filename)
+    rows = csv_user.read_csv()
+    for attribute in attribute_list:
+        if [attribute] not in rows:
+            csv_user.write_csv([attribute])
+            print(attribute, '--', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+            tbot.send_message(id_channel, text=attribute)
+            time.sleep(5)
+        else:
+            print('строка уже существует', '--', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+
+if __name__ == '__main__':
+    tbot = TelegramBot(token)
+
+    while True:
+        main(selenium=False)
         time.sleep(delay * 60)
